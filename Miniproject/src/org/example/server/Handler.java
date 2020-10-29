2e2ded0d2460d8e7ec10157668c48bde20891d4b
mergimgashi <56248317+mergimgashi@users.noreply.github.com>
2020-10-29 13:34:31 +0100

    Add Handler message method, and welcome
    fixes #22

--- Miniproject/src/org/example/server/Handler.java
+++ Miniproject/src/org/example/server/Handler.java
@@ -1,29 +1,25 @@
 package org.example.server;
 
 import java.net.Socket;
-import java.util.ArrayList;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Handler implements Runnable {
     
     private Socket socket;
-// Added client and server messages
-    private String clientMsg;
-    private String serverMsg;
-    private PrintWriter out;
     private int id;
     
-   
-
+    // Added client and server messages
+    private String clientMsg;
+    private String serverMsg;
+    private PrintWriter writer;
     private Lobby lobby = null;
     
-    public Handler(int id, Socket socket) throws IOException {
-        this.id = id;
-        this.socket = socket;  
-        this.out = new PrintWriter(socket.getOutputStream(), true);
+    Handler(int id, Socket socket) throws IOException {
+    	this.id = id;
+    	this.socket = socket;  
+    	this.writer = new PrintWriter(socket.getOutputStream(), true);
     }
 
     
@@ -31,10 +27,8 @@ public class Handler implements Runnable {
         try {
             // Instanced inStream, outStream and cmdHandler
             var inStream = new DataInputStream(socket.getInputStream());
-           
-           
             var cmdHandler = new CommandHandler();
-            
+
             //Loop for when cmdHandler is stopped
             while (!cmdHandler.isStopped()) {    
                 // read client message from instream to clientMsg 
@@ -42,12 +36,13 @@ public class Handler implements Runnable {
                 // read severMsg from cmDHandler process to serverMsg
                 serverMsg = cmdHandler.process(clientMsg, this);
                 // Use outStream to write server message to client
-                out.println(serverMsg);
+                if (serverMsg.length() > 0) {
+                	message(serverMsg);
+                }
             }
             
             // Wrap up and close operation
             inStream.close();
-           
             socket.close();
             
         } catch (IOException e) {
@@ -56,19 +51,23 @@ public class Handler implements Runnable {
         };
     }
     
+    public int getID() {
+    	return id;
+    }
+    
+    public void message(String msg) throws IOException {
+    	writer.println(msg);
+    }
+    
+    public void welcome() throws IOException {
+    	message("Welcome to the server");
+    }
+    
     public Lobby getLobby() {
     	return lobby;
     }
-   public PrintWriter getWriter() {
-	   return out;
-   }
    
-   public void setLobby(Lobby lobby) {
-	   this.lobby = lobby;
-   }
-   public int getID() {
-	    return id;
-	  }
-   
-    
+    public void setLobby(Lobby lobby) {
+	    this.lobby = lobby;
+    }
 }
--- Miniproject/src/org/example/server/NetworkService.java
+++ Miniproject/src/org/example/server/NetworkService.java
@@ -4,33 +4,47 @@ import java.net.ServerSocket;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 public class NetworkService implements Runnable {
+	
+	public static void main(String[] args) {
 
+		try {
+			var networkService = new NetworkService (6666, 10);
+			var serverThread = new Thread(networkService);
+			
+			serverThread.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static int nextID = 0;
 	private ServerSocket serverSocket;
 	private ExecutorService executorService;
-	private static int nextID = 0;
 
 	public NetworkService(int port, int poolSize)
-
 			throws IOException {
-
 		serverSocket = new ServerSocket(port);
 		executorService = Executors.newFixedThreadPool(poolSize);
 	}
 
 
 	public void run() {
-	    try {
-	        for(;;) {
-	            var nextSocket = serverSocket.accept();
-	            var handler = new Handler(nextID, nextSocket);                
-	            executorService.execute(handler);
-	            nextID = nextID+1;    
-	        }
-	    } catch(IOException e) {
-	      executorService.shutdown();
-	    }
+		try {
+
+			for(;;) {
+				var nextSocket = serverSocket.accept();
+				var handler = new Handler(nextID, nextSocket);				
+				executorService.execute(handler);
+				handler.message("Welcome to the server, Client no."+nextID+" - Current number of lobbies: " + Lobby.lobbies.size());
+				nextID = nextID+1;	
+			}
+		} catch(IOException e) {
+			executorService.shutdown();
+		}
 	}
 
 }
