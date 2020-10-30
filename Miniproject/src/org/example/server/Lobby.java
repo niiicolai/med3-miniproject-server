0f66b82646491d9553b24c4f475e44ad59b9e470
Slobodziuk <47206643+Slobodziuk@users.noreply.github.com>
2020-10-30 14:20:44 +0100

    Update Lobby.java
    Minor code changes.

--- Miniproject/src/org/example/server/Lobby.java
+++ Miniproject/src/org/example/server/Lobby.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Lobby {
-	
+
 	private int id = 0;
 
 	public ArrayList<Handler> handlers = new ArrayList<Handler>();
-	
+
 	public static int nextID = 0;
 	public static ArrayList<Lobby> lobbies = new ArrayList<Lobby>();
 
@@ -22,104 +22,105 @@ public class Lobby {
 	public int getID() {
 		return id;
 	}
-	
+
 	public ArrayList<Handler> getHandlers() {
 		return handlers;
 	}
-	
+
 	public void broadcast(Handler sender, String msg) {
-		  var message = sender.getFormattedName() +
-		          "(" + Date() + ")" + 
-		          ": " + msg;
-		        
-		  try {
-		    for (Handler handler : handlers) {    
-		      handler.message(message);    
-		    }
-		  } catch (IOException e) {
-		    e.printStackTrace();
-		  }
+		var message = sender.getFormattedName() + "(" + Date() + ")" + ": " + msg;
+
+		try {
+			for (Handler handler : handlers) {
+				handler.message(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	
+	}
+
 	public static String join(String str, Handler handler) {
-		try {  
-			 var id = Integer.parseInt(str.replaceAll("\\D+",""));
-	         return join(id, handler);
-	      } catch (NumberFormatException e) {  
-	         return "Please provide a ID";  
-	    }  
+		try {
+			var regex = "\\D+";
+			var numberStr = str.replaceAll(regex, "");
+			var id = Integer.parseInt(numberStr);
+			return join(id, handler);
+		} catch (NumberFormatException e) {
+			return "Please provide a ID";
+		}
 	}
-	
-	public static String join(int id, Handler handler) {    
-	    Lobby newLobby = null;
-	    for (int i = 0; i < lobbies.size(); i++) {
-	      var iLobby = lobbies.get(i);
-	      if (iLobby.getID() == id) {
-	        newLobby = iLobby;
-	        break;
-	      }
-	    }
-	    
-	    if (newLobby != null) {
-	    	newLobby.broadcast(handler, "joined the lobby");
-	    	
-	    	handler.setLobby(newLobby);
-	    	newLobby.getHandlers().add(handler);
-	    	return "Welcome to lobby " + newLobby.getID();
-	    } else {
-	    	return "Lobby not found!";
-	    }	    
+
+	public static String join(int id, Handler handler) {
+		Lobby newLobby = null;
+		for (int i = 0; i < lobbies.size(); i++) {
+			var iLobby = lobbies.get(i);
+			if (iLobby.getID() == id) {
+				newLobby = iLobby;
+				break;
+			}
+		}
+
+		if (newLobby != null) {
+			newLobby.broadcast(handler, "joined the lobby");
+
+			handler.setLobby(newLobby);
+			newLobby.getHandlers().add(handler);
+			return "Welcome to lobby " + newLobby.getID();
+		} else {
+			return "Lobby not found!";
+		}
 	}
-	
+
 	public static String find() {
-		
+
 		if (lobbies.size() == 0) {
 			return "No lobbies were found!";
 		}
-		
+
 		var newLineSymbol = System.getProperty("line.separator");
-	    var strBuilder = new StringBuilder();
-	    strBuilder.append("#n");
-	    strBuilder.append("Current lobbies: #n");
-	    for (int i = 0; i < lobbies.size(); i++) {
-	      strBuilder.append("Lobby ID: ");
-	      strBuilder.append(lobbies.get(i).getID());
-	      strBuilder.append("#n");
-	    }
-	    
-	    return strBuilder.toString();
+		var strBuilder = new StringBuilder();
+		strBuilder.append("#n");
+		strBuilder.append("Current lobbies: #n");
+		for (int i = 0; i < lobbies.size(); i++) {
+			strBuilder.append("Lobby ID: ");
+			strBuilder.append(lobbies.get(i).getID());
+			strBuilder.append("#n");
+		}
+
+		return strBuilder.toString();
 	}
-	
+
 	public static String create(Handler handler) {
 		var lobby = new Lobby(nextID);
-	    lobbies.add(lobby);
-	    handler.setLobby(lobby);
-	    lobby.getHandlers().add(handler);
-	    nextID++;
-	    
-	    return "Welcome to lobby " + lobby.getID();
+		lobbies.add(lobby);
+		handler.setLobby(lobby);
+		lobby.getHandlers().add(handler);
+		nextID++;
+
+		return "Welcome to lobby " + lobby.getID();
 	}
-	
+
 	public static String leave(Handler handler) {
 		var currentLobby = handler.getLobby();
-		if (currentLobby == null) return "Currently not in a lobby";
+		if (currentLobby == null)
+			return "Currently not in a lobby";
 
-    	currentLobby.getHandlers().remove(handler);
-    	handler.setLobby(null);
-		
+		currentLobby.getHandlers().remove(handler);
+		handler.setLobby(null);
+
 		if (currentLobby.getHandlers().size() == 0) {
 			lobbies.remove(currentLobby);
 		} else {
 			currentLobby.broadcast(handler, "left the lobby");
 		}
 
-    	return "Lobby was quitted!";
+		return "Lobby was quitted!";
 	}
-	
+
 	public String Date() {
-		  Date date = new Date();
-		  SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		  return formatter.format(date);
-		}
-	
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		return formatter.format(date);
+	}
+
 }
